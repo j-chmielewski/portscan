b1c8ef9a6185a5f3aa36971f2abb8ed4ab38cceb
Jacek Chmielewski <jchmielewski@teonite.com>
2020-10-12 13:03:04 +0200

    Target structure and parse_args function. I don't know what I'm doing...

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,32 @@ use std::net;
 static PORT_START: i32 = 1;
 static PORT_END: i32 = 100;
 
-fn main() {
+struct Target {
+    host: String,
+    port_start: i32,
+    port_end: i32
+}
+
+fn parse_args() -> Target {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("Usage:\n{} target", args[0]);
         process::exit(1);
     }
-    let target = &args[1];
-    println!("Scanning host {} ports {}-{}", target, PORT_START, PORT_END);
+    let target = args[1].clone();
+    Target {
+        host: target,
+        port_start: PORT_START,
+        port_end: PORT_END
+    }
+}
+
+fn main() {
+
+    let target = parse_args();
+    println!("Scanning host {} ports {}-{}", target.host, target.port_start, target.port_end);
     for port in PORT_START..PORT_END {
-        if let Ok(_) = net::TcpStream::connect(format!("{}:{}", target, port)) {
+        if let Ok(_) = net::TcpStream::connect(format!("{}:{}", target.host, port)) {
             println!("Found open port: {}", port);
         }
     }
