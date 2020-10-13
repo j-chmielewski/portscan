95ff5cdae19a49fab23f2a14fd3f7157b3843ca2
Jacek Chmielewski <jchmielewski@teonite.com>
2020-10-13 11:47:27 +0200

    Refactor argument parser to use match expression.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::net;
 use colored::*;
 
 static PORT_START: i32 = 1;
-static PORT_END: i32 = 100;
+static PORT_END: i32 = 65535;
 
 struct Target {
     host: String,
@@ -14,28 +14,28 @@ struct Target {
 
 fn parse_args() -> Target {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
-          return Target {
+    match args.len() {
+        2 => Target {
             host: args[1].clone(),
             port_start: PORT_START,
             port_end: PORT_END
-        }
-   } else if args.len() == 4 {
-         return Target {
+        },
+        4 => Target {
             host: args[1].clone(),
             port_start: args[2].parse::<i32>().unwrap(),
             port_end: args[3].parse::<i32>().unwrap()
+        },
+        _ => {
+            println!("Usage:\n{} target [port_start port_end]", args[0]);
+            process::exit(1);
         }
-    } else {
-        println!("Usage:\n{} target [port_start port_end]", args[0]);
-        process::exit(1);
     }
 }
 
 fn main() {
     let target = parse_args();
     println!("Scanning host {} ports {}-{}", target.host.bright_green(), target.port_start.to_string().bright_green(), target.port_end.to_string().bright_green());
-    for port in PORT_START..PORT_END {
+    for port in target.port_start..target.port_end {
         if let Ok(_) = net::TcpStream::connect(format!("{}:{}", target.host, port)) {
             println!("Found open port: {}", port.to_string().bright_green());
         }
